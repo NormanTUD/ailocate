5f463490438d56a8b35b5bb1fcb81a311a2366d4
Norman Koch <norman.koch@tu-dresden.de>
2022-07-13 17:51:30 +0200

    dont re-index already checked files

--- create_index.sh
+++ create_index.sh
@@ -6,12 +6,15 @@ echo "Creating index"
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-for item in $(locate jpg | tac); do
-#for item in $(locate jpg; locate png; locate jpeg; locate gif); do
+for item in $(locate jpg; locate JPG; locate png; locate PNG; locate jpeg; locate JPEG; locate gif; locate GIF); do
 	echo "Indexing $item"
 	echo ""
 
-	predictions=$(python3 predict_single_file.py $item | grep -v xmin | sed -e 's/.*\s//' | sort | uniq | paste -d ":"  - - | sed -e 's/:$//')
+	if grep -q "$item" ~/.ailocate_index; then
+		echo "$item already indexe in ~/.ailocate_index"
+	else
+		predictions=$(python3 predict_single_file.py $item | grep -v xmin | sed -e 's/.*\s//' | sort | uniq | paste -d ":"  - - | sed -e 's/:$//')
+	fi
 
 	echo ">>>>>>>>>>>>>>>"
 	echo "$item;$predictions"
